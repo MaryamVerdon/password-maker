c67a7f2896f800c4e3ec97c23d327e162e344703
Maryam Verdon <maryamverdon@gmail.com>
2022-08-29 00:35:24 +0200

    générer le mot de passe

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const dataUppercase = dataLowercase.toUpperCase();
 const dataNumbers = "123456789";
 const dataSymbols = "&~\"'^*$ù,;!?(-_éèç)";
 
-const generatePassword = () => {};
+const rangeValue = document.getElementById("password-length");
+const generatePassword = () => {
+  let data = [];
+  let password = "";
+
+  if (lowercase.checked) data.push(...dataLowercase);
+  if (uppercase.checked) data.push(...dataUppercase);
+  if (numbers.checked) data.push(...dataNumbers);
+  if (symbols.checked) data.push(...dataSymbols);
+
+  if (data.length === 0) {
+    alert("Veuillez sélectionner des critères.");
+    return;
+  }
+
+  for (let i = 0; i < rangeValue.value; i++) {
+    password += data[Math.floor(Math.random() * data.length)];
+  }
+};
 
 generateButton.addEventListener("click", generatePassword);
