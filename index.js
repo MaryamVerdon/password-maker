74451dc0756aca2f621e19465ffa19876fa9cf46
Maryam Verdon <maryamverdon@gmail.com>
2022-08-29 00:16:36 +0200

    création des données autorisées

---
+++ index.js
@@ -0,0 +1,8 @@
+const dataLowercase = "azertyuiopqsdfghjklmwxcvbn";
+const dataUppercase = dataLowercase.toUpperCase();
+const dataNumbers = "123456789";
+const dataSymbols = "&~\"'^*$ù,;!?(-_éèç)";
+
+const generatePassword = () => {};
+
+generateButton.addEventListener("click", generatePassword);
